46d8de79541a656a0f04d8f39e71b586f4a25b64
Akkel-i <118986817+Akkel-i@users.noreply.github.com>
2024-05-06 19:02:25 +0300

    added stats and speed control

--- engine.js
+++ engine.js
@@ -1,3 +1,4 @@
+
 function make2DArray(columns, rows) {
     let array = new Array(columns);
     for (let i = 0; i < array.length; i++) {
@@ -11,8 +12,23 @@ let columns;
 let rows;
 let resolution = 10;
 
+//graaffin piirtämiseen
+let width = 1500;
+let height = 1000;
+let graph = new Array(width);
+let time = width;
+let graphsize = 1;
+
+let FPS = 60;
+
+
+
+for ( i = 0 ; i < width ; i ++ ) {
+    graph[i] = 0;
+  }
+
 function setup() {
-    createCanvas(1200, 800);
+    createCanvas(1500, 1200);
     columns = width / resolution;
     rows = height / resolution;
 
@@ -22,6 +38,7 @@ function setup() {
             grid[i][j] = floor(random(2));
         }
     }
+    frameRate(FPS);
 }
 
 function draw() {
@@ -60,6 +77,28 @@ function draw() {
     }
     grid = next_gen;
     clickToAddCell(next_gen);
+
+    // laskee montako minkäväristä solua on
+    var white = howManyCellsAndDrawGraf(grid);
+    // graaffi kokeilua
+    fill(200);
+    textSize(20);
+    text("FPS : " + FPS, 10, 50);
+    text("Amount of white cells : " + white, 10, 20);
+    text("Amount of white cells", width - 200, height + 30);
+    text("Time: " + (int) (millis()/1000) + "s", 10, height + 30);
+    graph[width-1] = white/2;
+    text(white, width - 50, height + 180 - graph[width-1]/graphsize);
+    stroke(255);
+    for ( i = 1 ; i < width ; i ++ ) {
+      point( i, height + 200 - graph[i]/graphsize);
+      graph[i-1] = graph[i];
+    }
+    //console.log(graphsize);
+    if ( white / graphsize > 350 ) {
+      graphsize ++;
+    }
+    noStroke();
 }
 
 function countNeighbors(grid, x, y) {
@@ -82,8 +121,9 @@ function clickToAddCell(next_gen) {
     const canvas = document.getElementById("defaultCanvas0");
 
     // Add a click event listener to the canvas
-    canvas.addEventListener("click", function (event) {
-        // This function will be called when the canvas is clicked
+    //canvas.addEventListener("click", function (event) {
+    canvas.addEventListener("mousedown", function (event) {
+        // This function will be called when the canvas is clicked (click), or pressed (mousedown) - paitsi ei toimi koska miten rakennettu
         // You can access the mouse click coordinates using event.clientX and event.clientY
         const mouseX = event.clientX - canvas.getBoundingClientRect().left;
         const mouseY = event.clientY - canvas.getBoundingClientRect().top;
@@ -101,4 +141,38 @@ function clickToAddCell(next_gen) {
         }
 
     });
+}
+
+function changeSpeed(speed) {
+    //console.log(speed);
+    if (speed == 1) {
+        FPS = 1;
+        frameRate(FPS);
+    }
+    if (speed == 2) {
+        FPS = 20;
+        frameRate(FPS);
+    }
+    if (speed == 3) {
+        FPS = 500;
+        frameRate(FPS);
+    }
+}
+
+function howManyCellsAndDrawGraf(grid) {
+    let white = 0;
+    let black = 0;
+
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (grid[i][j] == 0) {
+                black++;
+            } else if (grid[i][j] == 1) {
+                white++;
+            }
+        }
+    }
+    //console.log("valkosia on: " + white);
+    //console.log("mustia on: " + black);
+    return white;
 }
